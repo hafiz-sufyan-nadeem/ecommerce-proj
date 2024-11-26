324365fd5c8696db40c4acc6013799c5eb5ef3cf
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2024-11-27 02:48:31 +0500

    I have set the index file, removed all the options from the sidebar, and added a table file where I have removed all the options and only kept the products.

--- resources/views/admin/layouts/index.blade.php
+++ resources/views/admin/layouts/index.blade.php
@@ -53,11 +53,11 @@
 
 
             <!-- Nav Item - Pages Collapse Menu -->
-            <li class="nav-item">
-                <a class="nav-link collapsed" href="#" >
-                    <i class="fas fa-fw fa-cog"></i>
-                    <span>Products</span>
-                </a>
+{{--            <li class="nav-item">--}}
+{{--                <a class="nav-link collapsed" href="#" >--}}
+{{--                    <i class="fas fa-fw fa-cog"></i>--}}
+{{--                    <span>Products</span>--}}
+{{--                </a>--}}
 
 
         </ul>
--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,7 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('admin.layouts.index');
 });
+
+Route::get('/tables', function () {
+    return view('admin.layouts.tables');
+});
