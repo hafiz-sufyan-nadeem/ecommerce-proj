545c733a434b4a9ecc68b877af4981354ad8c03b
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-01-12 03:00:11 +0500

    add to cart process

--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -548,7 +548,7 @@
 
                   </figure>
                   <div class="d-flex flex-column text-center">
-                    <h3 class="fs-6 fw-normal">Greek Style Plain Yogurt</h3>
+                    <h3 class="fs-6 fw-normal">{{$product->name}}</h3>
                     <div>
                       <span class="rating">
                         <svg width="18" height="18" class="text-warning"><use xlink:href="#star-full"></use></svg>
@@ -565,11 +565,17 @@
                       <span class="badge border border-dark-subtle rounded-0 fw-normal px-1 fs-7 lh-1 text-body-tertiary">10% OFF</span>
                     </div>
                     <div class="button-area p-3 pt-0">
-                      <div class="row g-1 mt-2">
+                      <div class="row g-1 mt-2" products_meta>
                           <div class="col-3">
-                              <input type="number" name="quantity" class="form-control border-dark-subtle input-number quantity" value="1" min="0">
+                              <input type="number" name="quantity" class="form-control border-dark-subtle input-number quantity selected_quantity" value="1" min="0">
+                              @auth
+                                  <a data-productId="{{$product->id}}" class="btn btn-primary rounded-1 p-2 fs-7 btn-cart add_cart" ><svg width="18" height="18"><use xlink:href="#cart"></use></svg> Add to Cart</a>
+                              @endauth
+
+                              @guest
+                                  <a href="{{route('login')}}"  class="btn btn-primary rounded-1 p-2 fs-7 btn-cart add_cart" > Login </a>
+                              @endguest
                           </div>
-                        <div class="col-7"><a href="{{route('add.to.cart',$product->id)}}" class="btn btn-primary rounded-1 p-2 fs-7 btn-cart"><svg width="18" height="18"><use xlink:href="#cart"></use></svg> Add to Cart</a></div>
                         <div class="col-2"><a href="#" class="btn btn-outline-dark rounded-1 p-2 fs-6"><svg width="18" height="18"><use xlink:href="#heart"></use></svg></a></div>
                       </div>
                     </div>
