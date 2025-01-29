f7b2d888c839c1afc3e8dfeeb5cd0c8a963e0d05
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-01-29 17:01:02 +0500

    remove extra things on checkout form

--- app/Http/Controllers/CartItemController.php
+++ app/Http/Controllers/CartItemController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\CartItem;
 use App\Models\Product;
-use App\Models\User;
 
 class CartItemController extends Controller
 {
--- resources/views/website/checkout.blade.php
+++ resources/views/website/checkout.blade.php
@@ -572,32 +572,14 @@
                 <div class="row">
                     <div class="col-md-6 mb-3">
                         <label for="firstName">First name</label>
-                        <input type="text" class="form-control" id="firstName" placeholder="" value="" required>
-                        <div class="invalid-feedback">
-                            Valid first name is required.
-                        </div>
                     </div>
+
                     <div class="col-md-6 mb-3">
                         <label for="lastName">Last name</label>
-                        <input type="text" class="form-control" id="lastName" placeholder="" value="" required>
-                        <div class="invalid-feedback">
-                            Valid last name is required.
-                        </div>
                     </div>
                 </div>
 
-                <div class="mb-3">
-                    <label for="username">Username</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text">@</span>
-                        </div>
-                        <input type="text" class="form-control" id="username" placeholder="Username" required>
-                        <div class="invalid-feedback" style="width: 100%;">
-                            Your username is required.
-                        </div>
-                    </div>
-                </div>
+
 
                 <div class="mb-3">
                     <label for="email">Email <span class="text-muted">(Optional)</span></label>
@@ -645,23 +627,17 @@
                             Please select a valid country.
                         </div>
                     </div>
-                    <div class="col-md-4 mb-3">
-                        <label for="state">State</label>
-                        <select class="custom-select d-block w-100" id="state" required>
-                            <option value="">Choose...</option>
-                            <option>California</option>
-                        </select>
-                        <div class="invalid-feedback">
-                            Please provide a valid state.
-                        </div>
-                    </div>
-                    <div class="col-md-3 mb-3">
-                        <label for="zip">Zip</label>
-                        <input type="text" class="form-control" id="zip" placeholder="" required>
-                        <div class="invalid-feedback">
-                            Zip code required.
-                        </div>
-                    </div>
+{{--                    <div class="col-md-4 mb-3">--}}
+{{--                        <label for="state">State</label>--}}
+{{--                        <select class="custom-select d-block w-100" id="state" required>--}}
+{{--                            <option value="">Choose...</option>--}}
+{{--                            <option>California</option>--}}
+{{--                        </select>--}}
+{{--                        <div class="invalid-feedback">--}}
+{{--                            Please provide a valid state.--}}
+{{--                        </div>--}}
+{{--                    </div>--}}
+
                 </div>
                 <hr class="mb-4">
                 <div class="custom-control custom-checkbox">
