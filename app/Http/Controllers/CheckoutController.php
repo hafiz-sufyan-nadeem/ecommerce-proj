63a2f1b397b0430461369fe5e849ee1528c72cff
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-01-31 23:51:42 +0500

    display continue to checkout button for only user login and only his form will display

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -12,14 +12,14 @@ class CheckoutController extends Controller
 {
     public function checkout(Request $request) {
         dd($request->all());
-        $userId = $request->user_id; // URL se user ID le raha hai
+        $userId = $request->user_id;
 
-        // User ke cart items fetch karo
+        // User ke cart items fetch kro
         $cartItems = CartItem::where('user_id', $userId)->with('product')->get();
 
-//        if ($cartItems->isEmpty()) {
-//            return redirect()->back()->with('error', 'Your cart is empty.');
-//        }
+        if ($cartItems->isEmpty()) {
+            return redirect()->back()->with('error', 'Your Cart is empty.');
+        }
 
         return view('website.checkout', [
             'cartItems' => $cartItems,
