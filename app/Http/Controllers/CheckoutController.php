0bf84833ebdf34eda300bec9ae5fb0ed64f3960b
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-02-02 20:11:10 +0500

    displaying user first and last name on checkout Form

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -11,6 +11,7 @@ use Illuminate\Support\Facades\DB;
 class CheckoutController extends Controller
 {
     public function checkout(Request $request) {
+        $user = auth()->user();
         $userId = $request->user_id;
 
         $cartItems = CartItem::where('user_id', $userId)->with('product')->get();
@@ -24,6 +25,7 @@ class CheckoutController extends Controller
         $totalPrice = $cartItems->sum('total_price'); // Price Total
 
         return view('website.checkout',[
+            'user' => $user,
             'cartItems' => $cartItems,
             'products' => $products,
             'totalPrice' => $totalPrice
--- resources/views/website/checkout.blade.php
+++ resources/views/website/checkout.blade.php
@@ -561,11 +561,11 @@
             <form class="needs-validation" novalidate>
                 <div class="row">
                     <div class="col-md-6 mb-3">
-                        <label for="firstName">First name</label>
+                        <label for="firstName"> <b>First Name:</b> {{ $user->name }}</label>
                     </div>
 
                     <div class="col-md-6 mb-3">
-                        <label for="lastName">Last name</label>
+                        <label for="lastName"> <b>Last Name:</b> {{ $user->last_name }}</label>
                     </div>
                 </div>
 
