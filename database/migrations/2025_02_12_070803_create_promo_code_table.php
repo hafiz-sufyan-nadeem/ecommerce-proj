2beae37799de8f20a1780e8a7db0ae5aa3d3676c
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-02-12 12:21:45 +0500

    add columns in promo_code table

--- database/migrations/2025_02_12_070803_create_promo_code_table.php
+++ database/migrations/2025_02_12_070803_create_promo_code_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('promo_code', function (Blueprint $table) {
             $table->id();
+            $table->string('code');
+            $table->decimal('discount_percentage')->nullable();
             $table->timestamps();
         });
     }
