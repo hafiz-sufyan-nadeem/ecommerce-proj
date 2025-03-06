6a05787c138b3f8b3ec9a775987101e1d37a1d1c
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-03-06 06:26:39 +0500

    set the DB order

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Order;
 
 class OrderController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'string',
+            'last_name' => 'string',
+            'email' => 'email',
+            'total_price' => 'numeric'
+        ]);
+
+        // Order Create Karein
+        $order = new Order();
+        $order->first_name = $request->first_name;
+        $order->last_name = $request->last_name;
+        $order->email = $request->email;
+        $order->address = $request->address;
+        $order->total_price = $request->total_price;
+        $order->save();
+
+        return response()->json(['success' => true, 'message' => 'Order placed successfully!']);
+    }
 }
+
