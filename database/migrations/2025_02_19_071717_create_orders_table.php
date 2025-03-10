135200fa4f13398eea928a0938abdda4bd407c8e
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-03-10 21:25:01 +0500

    make user id table in orders

--- database/migrations/2025_02_19_071717_create_orders_table.php
+++ database/migrations/2025_02_19_071717_create_orders_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
             $table->string('last_name');
             $table->string('email');
             $table->decimal('total_price');
--- database/migrations/2025_03_06_015446_add_user_id_to_orders_table.php
+++ database/migrations/2025_03_06_015446_add_user_id_to_orders_table.php
@@ -9,20 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::table('orders', function (Blueprint $table) {
-            //
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
         });
     }
+
 };
