8eb4c802b336e858e4f40adf5206cae057d05d8a
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-03-15 16:08:56 +0500

    again migrate all tables without any data

--- database/migrations/2025_03_06_015446_add_user_id_to_orders_table.php
+++ database/migrations/2025_03_06_015446_add_user_id_to_orders_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('orders', function (Blueprint $table) {
-            if (!Schema::hasColumn('orders', 'user_id')) {
+            if (!Schema::hasColumn('orders', 'user_id')) { // ✅ Pehle check karega
                 $table->unsignedBigInteger('user_id')->after('id');
             }
         });
