e58b2af5d3e5d5b48da5b523d22e3c099c3122b5
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-03-15 16:27:37 +0500

    update is_admin code

--- database/migrations/2025_03_15_112435_add_is_admin_to_users_table.php
+++ database/migrations/2025_03_15_112435_add_is_admin_to_users_table.php
@@ -9,20 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('is_admin')->default(1);
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_admin');
         });
     }
 };
