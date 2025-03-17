6c2dfdb5db71c83da1e59c2159a84b6bd04597ac
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-03-17 16:22:01 +0500

    add logic for access to admin panel

--- database/migrations/2025_03_17_111945_add_is_admin_to_users_table.php
+++ database/migrations/2025_03_17_111945_add_is_admin_to_users_table.php
@@ -9,20 +9,21 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->boolean('is_admin')->default(0)->after('email');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
+
+    public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->dropColumn('is_admin');
         });
     }
 };
