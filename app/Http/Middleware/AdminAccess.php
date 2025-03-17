eca1466bca8f62f20f8dc584e42854f071e90ea1
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-03-17 16:51:49 +0500

    working on tables

--- app/Http/Middleware/AdminAccess.php
+++ app/Http/Middleware/AdminAccess.php
@@ -5,19 +5,17 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAccess
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
     public function handle(Request $request, Closure $next): Response
     {
-        if(auth()->check() && Auth()->user()->is_admin == 1){
+        if (Auth::check() && Auth::user()->is_admin == 1) {
             return $next($request);
         }
-        return redirect()->route('home');
+
+        return redirect()->route('home')->with('error', 'Access Denied! You are not an admin.');
     }
 }
+
