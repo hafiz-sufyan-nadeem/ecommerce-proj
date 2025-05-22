6926f870e5bf6aa2698336a40cfda0c404acc0ee
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-05-22 13:42:38 +0500

    fill the model data of order

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $fillable = [
+        'user_id', 'name', 'last_name', 'email', 'address',
+        'country', 'payment_method', 'card_name', 'card_number', 'card_expiration', 'card_cvv',
+    ];
+
 }
