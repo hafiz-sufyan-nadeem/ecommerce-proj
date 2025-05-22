a2cf25c8e00166790987bd4afdfededcca2fe31c
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-05-22 13:38:20 +0500

    make order model

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -32,4 +32,38 @@ class CheckoutController extends Controller
         ]);
     }
 
+    public function store(Request $request)
+    {
+        $request->validate([
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|email',
+            'address' => 'required',
+            'country' => 'required',
+            'payment_method' => 'required',
+            'card_name' => 'required',
+            'card_number' => 'required',
+            'card_expiration' => 'required',
+            'card_cvv' => 'required',
+        ]);
+
+        Order::create([
+            'user_id' => Auth::id(),  // assuming user login hai
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'address' => $request->address,
+            'address2' => $request->address2,
+            'country' => $request->country,
+            'payment_method' => $request->payment_method,
+            'card_name' => $request->card_name,
+            'card_number' => $request->card_number,
+            'card_expiration' => $request->card_expiration,
+            'card_cvv' => $request->card_cvv,
+        ]);
+
+        return redirect()->back()->with('success', 'Order placed successfully!');
+    }
+
+
 }
--- database/migrations/2025_05_22_082628_create_orders_table.php
+++ database/migrations/2025_05_22_082628_create_orders_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('address');
+            $table->string('country');
+            $table->string('payment_method');
+            $table->string('card_name');
+            $table->string('card_number');
+            $table->string('card_expiration');
+            $table->string('card_cvv');
             $table->timestamps();
         });
     }
