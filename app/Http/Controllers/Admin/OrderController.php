9380ef03211733b0cd103e0a4bca5565059a6e8f
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-06-25 01:14:26 +0500

    make index file in admin->orders->index

--- app/Http/Controllers/Admin/OrderController.php
+++ app/Http/Controllers/Admin/OrderController.php
@@ -3,9 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function index()
+    {
+        $orders = Order::latest()->get();
+
+        return view('admin.orders.index', compact('orders'));
+    }
 }
