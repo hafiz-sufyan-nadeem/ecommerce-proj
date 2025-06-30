123069ea161c14e4593f6386f428f0a3bb709cc5
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-07-01 00:08:48 +0500

    make relations of product , user with wishlists

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
         return $this->hasMany(Review::class, 'user_id');
     }
 
+    public function wishlists()
+    {
+        return $this->hasMany(wishlist::class);
+    }
+
 }
--- app/Models/product.php
+++ app/Models/product.php
@@ -33,4 +33,9 @@ class Product extends Model
         return $this->reviews()->avg('rating');
     }
 
+    public function wishlists()
+    {
+        return $this->hasMany(wishlist::class);
+    }
+
 }
