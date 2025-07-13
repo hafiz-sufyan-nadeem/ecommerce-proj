9d138f882a61c97935f4d95d1227e05de1e59ef8
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-07-13 17:48:34 +0500

    make relation in orderItem model

--- app/Models/OrderItem.php
+++ app/Models/OrderItem.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class OrderItem extends Model
 {
-    //
+    protected $fillable = [
+        'order_id', 'product_id', 'quantity', 'price', 'total_price'
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
