85735f0b5d5d9cd19082ab852f7a6897eec31e4b
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-07-13 17:46:32 +0500

    add field in orderItem

--- database/migrations/2025_07_13_124430_create_order_items_table.php
+++ database/migrations/2025_07_13_124430_create_order_items_table.php
@@ -13,7 +13,13 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('order_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->integer('total_price');
             $table->timestamps();
+
         });
     }
 
