00da9f9601bbd96cc653caec59d0ff68f3c27453
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-07-23 18:30:14 +0500

    display imgs of categoris on viewall page

--- resources/views/website/categories.blade.php
+++ resources/views/website/categories.blade.php
@@ -6,6 +6,11 @@
         @foreach ($categories as $category)
             <div class="category-box">
                 <h3 style="color: #e74a3b">{{ $category->name }}</h3>
+
+                <div class="col-md-6 text-center mb-2">
+                <img src="{{asset('/admin-images/category/'. $category->image)}}" class="rounded-circle" alt="Category Thumbnail" style="width: 160px"; height="160px";u>
+                 </div>
+
                 <a href="{{ route('category.products', $category->id) }}" class="btn btn-primary">View Products</a>
             </div>
         @endforeach
