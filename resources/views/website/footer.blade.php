7b1f7be4d725f349d10c7e0f4a32223ff6fa2e75
hafiz-sufyan-nadeem <hafizsufyan398@gmail.com>
2025-07-23 16:36:43 +0500

    remove the copyright extra code

--- resources/views/website/footer.blade.php
+++ resources/views/website/footer.blade.php
@@ -132,9 +132,7 @@
             <div class="col-md-6 copyright">
                 <p>© 2024 Organic. All rights reserved.</p>
             </div>
-            <div class="col-md-6 credit-link text-start text-md-end">
-                <p>HTML Template by <a href="https://templatesjungle.com/">TemplatesJungle</a> Distributed By <a href="https://themewagon.com">ThemeWagon</a> </p>
-            </div>
+
         </div>
     </div>
 </div>
